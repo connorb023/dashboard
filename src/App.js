f4e51282b0a4da72cb70f3dbc4307ca769d7cbba
connorb023 <connorcb@yahoo.com>
2023-05-08 19:58:44 -0600

    app.js fixes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Reviews from './components/Reviews';
 import Rating from './components/Rating';
 import SentimentAnalysis from './components/SentimentAnalysis';
 import Visitors from './components/Visitors';
-import './styles/App.css';
-
 
 function App() {
   return (
     <BrowserRouter>
-      <Sidebar />
-      <Route path="/" exact component={Reviews} />
-      <Route path="/rating" component={Rating} />
-      <Route path="/sentiment" component={SentimentAnalysis} />
-      <Route path="/visitors" component={Visitors} />
+      <div className="app">
+        <Sidebar />
+        <div className="main">
+          <Route path="/" exact component={Reviews} />
+          <Route path="/rating" component={Rating} />
+          <Route path="/sentiment" component={SentimentAnalysis} />
+          <Route path="/visitors" component={Visitors} />
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
