920cf684b47a45d0d60530878892f632e78fcee5
connorb023 <connorcb@yahoo.com>
2023-05-08 20:26:17 -0600

    fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Reviews from './components/Reviews';
-import Rating from './components/Rating';
-import SentimentAnalysis from './components/SentimentAnalysis';
-import Visitors from './components/WebsiteVisitors';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Reviews from './Reviews';
+import AverageRating from './AverageRating';
+import SentimentAnalysis from './SentimentAnalysis';
+import WebsiteVisitors from './WebsiteVisitors';
+import Settings from './Settings';
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Sidebar />
-        <div className="main">
-          <Route path="/" exact component={Reviews} />
-          <Route path="/rating" component={Rating} />
-          <Route path="/sentiment" component={SentimentAnalysis} />
-          <Route path="/visitors" component={Visitors} />
-        </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Dashboard</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/reviews">Reviews</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/average-rating">Average Rating</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sentiment-analysis">Sentiment Analysis</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/website-visitors">Website Visitors</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/settings">Settings</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
+        <Switch>
+          <Route path="/" exact>
+            <h1>Dashboard</h1>
+          </Route>
+          <Route path="/reviews">
+            <Reviews />
+          </Route>
+          <Route path="/average-rating">
+            <AverageRating />
+          </Route>
+          <Route path="/sentiment-analysis">
+            <SentimentAnalysis />
+          </Route>
+          <Route path="/website-visitors">
+            <WebsiteVisitors />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   );
-}
+};
 
 export default App;
