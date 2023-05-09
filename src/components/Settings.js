84296c00b9aacf5183e4490516de951e0d7caab4
connorb023 <connorcb@yahoo.com>
2023-05-08 20:24:53 -0600

    settings file

---
+++ src/components/Settings.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Here are some settings:</p>
+      <ul>
+        <li>Language: English</li>
+        <li>Timezone: GMT-5</li>
+        <li>Theme: Light</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Settings;
