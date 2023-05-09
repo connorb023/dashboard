27a796cef88ba50be791202141b0d9c70b661ea0
connorb023 <connorcb@yahoo.com>
2023-05-08 19:37:34 -0600

    router package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Reviews from './components/Reviews';
+import Rating from './components/Rating';
+import SentimentAnalysis from './components/SentimentAnalysis';
+import Visitors from './components/Visitors';
+import './styles/App.css';
+
 
 function App() {
   return (
